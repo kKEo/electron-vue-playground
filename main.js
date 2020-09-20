2bfd7408022d9263631f7e750609a3456fa88917
Krzysztof Maziarz <krzysztof.maziarz@gmail.com>
2020-09-20 21:50:50 +0200

    Turn into tray docked app

--- main.js
+++ main.js
@@ -1,40 +1,60 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, Menu, Tray, BrowserWindow} = require('electron')
 const path = require('path')
 
+
+let tray = null
+app.whenReady().then(() => {
+  tray = new Tray('assets/icons/tray.png')
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', click: openApp }
+  ])
+
+  tray.on("click", openApp)
+  tray.on("double-click", reloadApp)
+})
+
+let activeWindow
+
 function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
+  console.log("Create window")
+  mainWindow = new BrowserWindow({
+    width: 1600,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  let bounds = mainWindow.getBounds();
+  bounds.y = 40
+  bounds.x = bounds.x + 480
+  mainWindow.setBounds(bounds)
+  mainWindow.loadFile('apps/index.html')
+  mainWindow.webContents.openDevTools()
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  return mainWindow
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-  
+function reloadApp() {
+  console.log("reload app")
+  if (activeWindow !== undefined && !activeWindow.isDestroyed()){
+    activeWindow.destroy()
+  }
+  openApp()
+}
+
+function openApp() {
+  if (activeWindow === undefined || activeWindow.isDestroyed()) {
+    activeWindow = createWindow()
+  } else {
+    activeWindow.show()
+  }
+
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
-})
+}
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
